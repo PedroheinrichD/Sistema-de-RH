0f181c008d4a8625c695f6f5ecdae7af31920637
Pedro Heinrich <pedroheinrich91@gmail.com>
2025-10-22 06:53:33 -0300

    s

--- js.js
+++ js.js
@@ -1,12 +1,24 @@
 let media
-let funcionarios = {
+/* let funcionarios = {
     nome: '',
     cargo: '',
     nota1: 5,
     nota2: 8,
     nota3: 3
 }
+*/
 
+
+
+let funcionarios = [
+    {
+        nome: document.querySelector('#nome'),
+        cargo: document.querySelector('#cargo'),
+        nota1: document.querySelector('#nota1'),
+        nota2: document.querySelector('#nota2'),
+        nota3: document.querySelector('#nota3')
+    }
+]
 
 for (let i = 0; i < funcionarios.length; i++) {
     const elementos = funcionarios[i];
@@ -14,21 +26,28 @@ for (let i = 0; i < funcionarios.length; i++) {
 
 
 function calcMedia() {
-     media = (funcionarios.nota1 + funcionarios.nota2 + funcionarios.nota3) / 3
-     console.log(`Cálculo da média é: ${media.toFixed(1)}`);
+    media = (funcionarios.nota1 + funcionarios.nota2 + funcionarios.nota3) / 3
+    console.log(`Cálculo da média é: ${media.toFixed(1)}`);
 }
 
 
-function desempenho(){
-    if(media >= 8){
+function desempenho() {
+    if (media >= 8) {
         console.log(`Desempenho: Excelente!`);
-        
-    }else if(media >= 6){
+
+    } else if (media >= 6) {
         console.log(`Desempenho: Satifatório`);
-        
-    }else{
+
+    } else {
         console.log(`Desempenho: Necessista Treinamento`);
     }
 }
 
 
+
+
+function adicionarForm(event) {
+    event.preventDefault()
+}
+
+let meuFormulario = document.querySelector('#formFuncionario')
